f217f3bba716885859e67d900e6cef12c36ce203
Nícolas Deçordi <nicolas.decordi@cubos.io>
2018-09-04 23:49:23 -0300

    feat(components): add ContentBox to component indexes

--- app/components/index.js
+++ app/components/index.js
@@ -1,7 +1,9 @@
+import ContentBox from './ContentBox';
 import LocationBox from './LocationBox';
 import ReferenceList from './ReferenceList';
 
 export {
+    ContentBox,
     LocationBox,
     ReferenceList
 };
