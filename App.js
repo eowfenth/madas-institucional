192f7aee245b395609309cb6544e009f337abb56
Nícolas Deçordi <nicolas.decordi@cubos.io>
2018-09-05 20:38:20 -0300

    feat(app): add ClassScreen component

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello, World!</Text>
-      </View>
-    );
-  }
-}
+import { ScrollView, StyleSheet, View } from 'react-native';
+import {
+	ContentBox,
+	LocationBox,
+	ReferenceList,
+} from './app/components';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    }
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	}
 });
+
+const ClassScreen = () => (
+	<View style={{ flex: 1 }}>
+		<View
+			style={{
+				flex: 1,
+				marginTop: 5,
+				marginLeft: 10,
+				marginRight: 10,
+			}}
+		>
+			<ContentBox
+				title={example.title}
+				subtitle={example.subtitle}
+				speakers={example.speakers}
+				content={example.content}
+			/>
+			<ReferenceList references={example.references} />
+		</View>
+		<LocationBox
+			location={example.location}
+			complement={example.complement}
+			latitude={example.latitude}
+			longitude={example.longitude}
+			mapsUrl={example.mapsUrl}
+			startAt={example.startAt}
+			endAt={example.endAt}
+		/>
+	</View>
+);
+
+const App = () => (
+	<ScrollView>
+		<View style={styles.container}>
+			<ClassScreen />
+		</View>
+	</ScrollView>
+);
+
+export default App;
