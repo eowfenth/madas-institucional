32b19a2283ed080111d1cdf0b39c7ee1f2689fa3
Nícolas Deçordi <nicolas.decordi@cubos.io>
2018-09-04 22:08:30 -0300

    feat(components): add ReferenceList component

---
+++ app/components/ReferenceList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const labels = {
+    listName: "Materiais de Discussão:"
+};
+
+export default ReferenceList = (props) => (
+    <View>
+        <Text style={{ ...styles.genericLabel, fontSize: 16 }}>{labels.listName}</Text>
+        {props.references.map(x => (
+            <View key={x.key} style={styles.referenceItemContainer}>
+                <Text style={{}}>• {x.author}</Text>
+                <Text style={styles.referenceTitleLabel}>{x.title}</Text>
+                <Text>{x.from}</Text>
+            </View>
+        ))}
+    </View>
+);
+
+const styles = StyleSheet.create({
+    listLabel: {
+    },
+    genericLabel: {
+        color: "#212121"
+    },
+    referenceTitleLabel: {
+        marginTop: 5
+    },
+    referenceItemContainer: {
+        marginTop: 10
+    }
+});
